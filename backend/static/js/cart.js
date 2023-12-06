f598f1b65e78ae2294064da88186a8e048bb8f0e
Abhijith Subash <abhijithsubash07@gmail.com>
2023-12-06 22:27:47 +0530

    fix: add event listener for increase decrease button

--- backend/static/js/cart.js
+++ backend/static/js/cart.js
@@ -1,1 +1,10 @@
-console.log("Cart script added")
+var decreaseButton = document.querySelector(".btn-decrease")
+var increaseButton = document.querySelector(".btn-increase")
+
+decreaseButton.addEventListener("click", () => {
+    console.log("Decreased")
+})
+
+increaseButton.addEventListener("click", () => {
+    console.log("Increased")
+})
