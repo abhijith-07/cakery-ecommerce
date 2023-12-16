1f8df8fbc7c98fdcdcf38be387023585fee119d4
Abhijith Subash <abhijithsubash07@gmail.com>
2023-12-16 16:11:26 +0530

    fix: static urls for production debug fixed

--- backend/cakery/urls.py
+++ backend/cakery/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("", include('shop.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
