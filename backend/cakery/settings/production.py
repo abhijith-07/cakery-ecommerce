96ecc5a6c282c339abcb7db84d8b2fb843c2b2c5
Abhijith Subash <abhijithsubash07@gmail.com>
2023-12-16 16:44:05 +0530

    fix: production bug fixes, debug false

--- backend/cakery/settings/production.py
+++ backend/cakery/settings/production.py
@@ -1,8 +1,8 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 DATABASES = {
     "default": {
@@ -18,4 +18,4 @@ DATABASES = {
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'uploads')
