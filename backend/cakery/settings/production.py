f425f67af244402c53fb7611ca54fb0dc1023f4f
Abhijith Subash <abhijithsubash07@gmail.com>
2023-12-16 16:58:49 +0530

    fix: postgres changed to railway

--- backend/cakery/settings/production.py
+++ backend/cakery/settings/production.py
@@ -11,7 +11,7 @@ DATABASES = {
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"), 
-        'PORT': os.getenv("DB_PORT"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
