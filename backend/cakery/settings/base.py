f51faf5a7dbc27ecb1302f065606829951e027b9
Abhijith Subash <abhijithsubash07@gmail.com>
2023-12-17 11:48:08 +0530

    fix: whitenoise added

--- backend/cakery/settings/base.py
+++ backend/cakery/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -104,6 +105,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STORAGES = {
+    # WhiteNoise to compresses files and hashes
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 STATIC_URL = "static/"
 
